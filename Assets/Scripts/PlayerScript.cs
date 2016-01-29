6013a3b5beb8e1346ed44685b231c1a15ba20313
Student <student@ad.aie.edu>
2016-01-29 22:20:19 +1100

    Add basic player movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed;
+    public float mouseSpeed;
+
+    private Rigidbody rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Confined;
+	}
+
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 newPosition = transform.position;
+        newPosition += (transform.right * horizontalAxis + 
+            transform.forward * verticalAxis) * speed * Time.deltaTime;
+        rigidbody.MovePosition(newPosition);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(new Vector3(0.0f, mouseX, 0.0f));
+        
+    }
+}
