567b404a7831701641326e9ee825f16702b36493
Student <student@ad.aie.edu>
2016-01-30 15:10:06 +1100

    Add extinguishable triggers and an example maze

--- Assets/Scripts/TriggerController.cs
+++ Assets/Scripts/TriggerController.cs
@@ -1,30 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TriggerController : MonoBehaviour {
-
+public class TriggerController : MonoBehaviour
+{
     public Material onMaterial;
+    public Material offMaterial;
     public GameController gameController;
+    public ParticleSystem litFlame;
+    public float flameExtinguishDelay;
 
     private MeshRenderer meshRenderer;
     private bool isTriggered = false;
 
     void Start () {
         meshRenderer = GetComponent<MeshRenderer>();
+        litFlame.Stop();
 	}
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            isTriggered = true;
-            meshRenderer.material = onMaterial;
-            gameController.CheckForSunUp();
+            StartCoroutine(LightAndExtinguishFlame());
         }
     }
 
     public bool IsTriggered
     {
         get { return isTriggered; }
+    }
+
+    private IEnumerator LightAndExtinguishFlame()
+    {
+        // Lights the flame
+        isTriggered = true;
+        meshRenderer.material = onMaterial;
+        gameController.CheckForSunUp();
+        litFlame.time = 0;
+        litFlame.Play();
+
+        // Extinguishes the flame after the delay
+        yield return new WaitForSeconds(flameExtinguishDelay);
+        isTriggered = false;
+        meshRenderer.material = offMaterial;
+        if (litFlame.isPlaying)
+        {
+            litFlame.Stop();
+        }
     }
 }
